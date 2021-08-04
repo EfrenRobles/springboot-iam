e3fcd1c1e7e3574ed3857d554aa7adc34732745c
Efren Robles <efren.robles@srax.com>
2021-08-03 17:19:59 -0700

    ! Adding new project for IAM OAuth2 experimental.

---
+++ src/main/java/com/example/iam/LeoIamApplication.java
@@ -0,0 +1,13 @@
+package com.example.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoIamApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/iam/controller/HelloWorldController.java
@@ -0,0 +1,5 @@
+package com.example.iam.controller;
+
+public class HelloWorldController {
+
+}
